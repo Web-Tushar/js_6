ab705bd7fc754cb7bbe671411dc0da8180be61f1
Web-Tushar <tusharimranvip895@gmail.com>
2024-01-23 12:58:25 +0600

    for loop

---
+++ script.js
@@ -0,0 +1,10 @@
+// let result = document.querySelector(".result")
+
+// let students = ["imran", "tushar", "ali","anik","shawon","rayhan","rakib","mamun","hasib"]
+// // console.log(students.length)
+
+// for(let i = 0; i < students.length; i++){
+//      // result.innerHTML = (`${students[i]} ${i}`);
+     // console.log(`${students[i]} ${i}`)
+
+// }
